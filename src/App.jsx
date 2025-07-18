eb81ef7229eecf0191e10e70a6db17a6aa4da287
ginorojo <ginorojoj@gmail.com>
2025-07-17 21:09:30 -0400

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import Body from './components/Body.jsx'
+import { Helmet } from "react-helmet";
 export default function App() {
   return (
     <div className='bg-[#242424]'>
